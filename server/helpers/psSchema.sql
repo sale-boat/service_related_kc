158854e069960b92a3ec02fe58a5181817842033
khengschew <khengc@gmail.com>
2019-03-06 18:20:43 -0800

    Update database schemas and import scripts for optimizations

--- server/helpers/psSchema.sql
+++ server/helpers/psSchema.sql
@@ -4,23 +4,12 @@
 -- generateProducts.js (will require additional CSVs to generate)
 
 \timing on
-CREATE TABLE related (
-  related_id serial PRIMARY KEY,
-  src_prod_id integer,
-  rel_prod_id integer
-);
-
-COPY related(src_prod_id, rel_prod_id)
-FROM '/Users/Kheng/Dropbox/Backup/Hack Reactor/sdc/service_related_kc/dist/related.csv' DELIMITER ',' CSV;
-
-CREATE INDEX src_idx ON related (rel_prod_id) INCLUDE (related_id, src_prod_id);
-CREATE INDEX rel_idx ON related (rel_prod_id) INCLUDE (related_id, src_prod_id);
 
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   prod_id integer,
-  prod_name varchar(255), -- truncate to speed up?
-  category varchar(255), -- truncate to speed up?
+  prod_name varchar(255),
+  category varchar(255),
   manufacturer varchar(255),
   primary_image varchar(255),
   secondary_images text[],
@@ -44,3 +33,45 @@ FROM '/Users/Kheng/Dropbox/Backup/Hack Reactor/sdc/service_related_kc/dist/produ
 
 CREATE INDEX prod_id_idx ON products (prod_id);
 CREATE INDEX prod_name_idx ON products (prod_name);
+
+CREATE TABLE related (
+  related_id serial PRIMARY KEY,
+  src_prod_id integer,
+  rel_prod_id integer
+);
+
+COPY related(src_prod_id, rel_prod_id)
+FROM '/Users/Kheng/Dropbox/Backup/Hack Reactor/sdc/service_related_kc/dist/related.csv' DELIMITER ',' CSV;
+
+CREATE INDEX src_idx ON related (src_prod_id) INCLUDE (related_id, rel_prod_id);
+CREATE INDEX rel_idx ON related (rel_prod_id) INCLUDE (related_id, src_prod_id);
+
+CREATE TABLE products_nest (
+  id SERIAL PRIMARY KEY,
+  prod_id integer,
+  prod_name varchar(255),
+  category varchar(255),
+  manufacturer varchar(255),
+  primary_image varchar(255),
+  secondary_images text[],
+  review_one_star_count integer,
+  review_two_star_count integer,
+  review_three_star_count integer,
+  review_four_star_count integer,
+  review_five_star_count integer,
+  review_count integer,
+  question_count integer,
+  price varchar(20),
+  total_price varchar(20),
+  stock integer,
+  is_prime boolean,
+  prod_description text,
+  thumbnail_image varchar(255),
+  related integer[]
+);
+
+COPY products_nest(prod_id, prod_name, category, manufacturer, primary_image, secondary_images, review_one_star_count, review_two_star_count, review_three_star_count, review_four_star_count, review_five_star_count, review_count, question_count, price, total_price, stock, is_prime, prod_description, thumbnail_image, related)
+FROM '/Users/Kheng/Dropbox/Backup/Hack Reactor/sdc/service_related_kc/dist/products-nested.csv' DELIMITER ',' CSV HEADER;
+
+CREATE INDEX prod_id_idx ON products_nest (prod_id);
+CREATE INDEX prod_name_idx ON products_nest (prod_name);
